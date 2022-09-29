0a99d98b24f89fe9342fb3991ce7d445b4c7d78a
Anna Boldyreva <hannabl1988@gmail.com>
2022-09-30 00:25:31 +0600

    neew

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery"); //2. Стучимся к классу, для доступа к сл. этапу
+gallery.insertAdjacentHTML("beforeend", createGallaryMarkup(galleryItems)); //3. рендерим готовую разметку в dom
+
+
+// 1. начало создания разметки
+function createGallaryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+    `;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionPosition: "bottom",
+  captionsData: "alt",
+    captionDelay: 250,
+// close: false, не показывать кнопку "закрыть"
+});
+
+
