4d4747a103d39f8c8f293a1d48f8d69caad4eb48
Anna Boldyreva <hannabl1988@gmail.com>
2022-10-03 23:38:07 +0600

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,13 +4,9 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const prevTime = localStorage.getItem('videoplayer-current-time');
-
-if (prevTime) player.setCurrentTime(prevTime);
-
-player.on(
-  'timeupdate',
-  throttle(data => {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-  }, 1000)
+player.on('timeupdate',  throttle( e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+    }, 1000)
 );
+    
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
