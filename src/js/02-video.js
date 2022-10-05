4aa6ff987a920ea1ab83adea2593e1299626eb39
Anna Boldyreva <hannabl1988@gmail.com>
2022-10-05 21:45:19 +0600

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,18 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
 
 
 
-player.on(
-  'timeupdate',
-  throttle(data => {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-  }, 1000)
-);
+// player.on(
+//   'timeupdate',
+//   throttle(data => {
+//     localStorage.setItem('videoplayer-current-time', data.seconds);
+//   }, 1000)
+// );
 
-const prevTime = localStorage.getItem('videoplayer-current-time');
+// const prevTime = localStorage.getItem('videoplayer-current-time');
 
-if (prevTime) player.setCurrentTime(prevTime);
+// if (prevTime) player.setCurrentTime(prevTime);
